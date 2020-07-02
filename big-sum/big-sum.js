2de5505633b3755086580f67279baae1a3753e07
Choibumsu <bumsu0211@gmail.com>
2020-07-02 16:19:48 +0900

    Completed

--- big-sum/big-sum.js
+++ big-sum/big-sum.js
@@ -1,2 +1,45 @@
-const a = "9223372036854775807";
-const b = "9223372036854775808";
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const [a, b] = line.split(" ");
+
+  const getReversed = (num) => num.split("").map((v) => +v);
+
+  const reversedA = getReversed(a);
+  const reversedB = getReversed(b);
+
+  const res = [];
+  let left = 0;
+
+  while (reversedA.length !== 0 && reversedB.length !== 0) {
+    const numA = reversedA.pop();
+    const numB = reversedB.pop();
+
+    const tmp = numA + numB + left;
+    res.push(tmp % 10);
+    left = parseInt(tmp / 10);
+  }
+
+  const longNum = reversedA.length === 0 ? reversedB : reversedA;
+
+  while (longNum.length !== 0) {
+    const num = longNum.pop();
+
+    const tmp = num + left;
+    res.push(tmp % 10);
+    left = parseInt(tmp / 10);
+  }
+
+  if (left !== 0) res.push(left);
+
+  console.log(res.reverse().join(""));
+
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
